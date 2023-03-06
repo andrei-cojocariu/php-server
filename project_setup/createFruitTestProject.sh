3c6622242f13425ffb6a7462f3c4c660b87f68fe
andrei-cojocariu <cojocariu.andrei@gmail.com>
2023-03-06 10:32:51 +0200

    add create and update FruitTestProject menu

--- project_setup/createFruitTestProject.sh
+++ project_setup/createFruitTestProject.sh
@@ -7,18 +7,22 @@ function createFruitTestProject() {
 }
 
 function updateFruitTestProject() {
-    select yn in "full-update" "git-update" "recreate" "skip"; do
-      case $yn in
-        full-update )
-          return ;;
-        git-update )
-          return ;;
-        recreate )
-          return ;;
-        skip )
-          return
-      esac
-    done
+  echo "Full Update = redo database and code (git checkout, composer install etc)"
+  echo "Git Update = clear cache and code update"
+  echo "Recreate = removes project files and resets is (git checkout, composer install)"
+
+  select yn in "full-update" "git-update" "recreate" "skip"; do
+    case $yn in
+      full-update )
+        return ;;
+      git-update )
+        return ;;
+      recreate )
+        return ;;
+      skip )
+        return
+    esac
+  done
 
   return
 }
