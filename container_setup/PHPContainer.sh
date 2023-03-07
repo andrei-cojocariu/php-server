47ad138277d0fdd330564303a246a39e79464c14
andrei-cojocariu <cojocariu.andrei@gmail.com>
2023-03-07 17:11:16 +0200

    add php-intl

--- container_setup/PHPContainer.sh
+++ container_setup/PHPContainer.sh
@@ -21,6 +21,7 @@ function createPHPContainer() {
   lxc exec ${phpContainerName} -- apt-get update
   lxc exec ${phpContainerName} -- apt -y install php8.2
   lxc exec ${phpContainerName} -- apt-get install -y php8.2-cli php8.2-common php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
+  lxc exec ${phpContainerName} -- apt-get install -y php-intl
 
   #Install Curl
   lxc exec ${phpContainerName} -- apt -y install curl
